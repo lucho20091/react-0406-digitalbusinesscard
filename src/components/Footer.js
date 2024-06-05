8d65bf5e23604917bf7ee98ee9752bd675e29990
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-04 21:04:33 -0500

    yes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer(){
     return (
         <footer className="ComponentFooter">
             <div className="ComponentFooter__Icons">
-                <a target="_blank" rel="noreferrer" href="https://www.twitter.com"><FontAwesomeIcon className="ComponentFooter__Twitter" icon={faSquareTwitter} /></a>
-                <a target="_blank" rel="noreferrer" href="https://www.facebook.com"><FontAwesomeIcon className="ComponentFooter__Facebook" icon={faSquareFacebook} /></a>
-                <a target="_blank" rel="noreferrer" href="https://www.instagram.com"><FontAwesomeIcon className="ComponentFooter__Instagram" icon={faSquareInstagram} /></a>
-                <a target="_blank" rel="noreferrer"  href="https://www.github.com"><FontAwesomeIcon className="ComponentFooter__Github" icon={faSquareGithub} /></a>
+                <a target="_blank" rel="noreferrer" aria-label="Twitter" href="https://www.twitter.com"><FontAwesomeIcon className="ComponentFooter__Twitter" icon={faSquareTwitter} /></a>
+                <a target="_blank" rel="noreferrer" aria-label="Facebook" href="https://www.facebook.com"><FontAwesomeIcon className="ComponentFooter__Facebook" icon={faSquareFacebook} /></a>
+                <a target="_blank" rel="noreferrer" aria-label="Instagram" href="https://www.instagram.com"><FontAwesomeIcon className="ComponentFooter__Instagram" icon={faSquareInstagram} /></a>
+                <a target="_blank" rel="noreferrer" aria-label="Github" href="https://www.github.com"><FontAwesomeIcon className="ComponentFooter__Github" icon={faSquareGithub} /></a>
             </div>
         </footer> 
     )
