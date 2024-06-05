f1300dbe9398734463d8ea7d45e82a049aa10376
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-04 20:53:40 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ export default function Footer(){
     return (
         <footer className="ComponentFooter">
             <div className="ComponentFooter__Icons">
-                <a target="_blank" href="twitter.com"><FontAwesomeIcon className="ComponentFooter__Twitter" icon={faSquareTwitter} /></a>
-                <a target="_blank"  href="facebook.com"><FontAwesomeIcon className="ComponentFooter__Facebook" icon={faSquareFacebook} /></a>
-                <a target="_blank"  href="instagram.com"><FontAwesomeIcon className="ComponentFooter__Instagram" icon={faSquareInstagram} /></a>
-                <a target="_blank"  href="github.com"><FontAwesomeIcon className="ComponentFooter__Github" icon={faSquareGithub} /></a>
+                <a target="_blank" rel="noreferrer" href="https://www.twitter.com"><FontAwesomeIcon className="ComponentFooter__Twitter" icon={faSquareTwitter} /></a>
+                <a target="_blank" rel="noreferrer" href="https://www.facebook.com"><FontAwesomeIcon className="ComponentFooter__Facebook" icon={faSquareFacebook} /></a>
+                <a target="_blank" rel="noreferrer" href="https://www.instagram.com"><FontAwesomeIcon className="ComponentFooter__Instagram" icon={faSquareInstagram} /></a>
+                <a target="_blank" rel="noreferrer"  href="https://www.github.com"><FontAwesomeIcon className="ComponentFooter__Github" icon={faSquareGithub} /></a>
             </div>
-        </footer>
+        </footer> 
     )
 }
